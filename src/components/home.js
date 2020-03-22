c082dced7c52f42e642663f60d26857e53b8b923
shawnbmason <s.mason1993@yahoo.com>
2020-03-22 00:14:38 -0400

    app setup

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='home'>
+        <h1>Course Scheduler</h1>
+        <h2>React Redux</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
