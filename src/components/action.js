bd1ae22c39ab60e758516ce9bf97b7c5d847aa54
shawnbmason <s.mason1993@yahoo.com>
2020-04-02 21:10:35 -0400

    styled add remove button

--- src/components/action.js
+++ src/components/action.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import Icon from './icons';
 
 class Action extends Component {
+
+  handleAction = function() {
+    this.props.onClick()
+
+    // document.getElementById('action').classList.add('')
+  }.bind(this);
+
   render() {
     return (
-      <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-        { Icon('fas fa-plus-circle') }
-        {/* 'far fa-times-circle' */}
-      </button>
+      <a
+        id='action'
+        onClick={() => this.handleAction()}
+        className={`${this.props.className} action`}>
+      </a>
     );
+    // return (
+    //   <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+    //     { Icon('fas fa-plus-circle') }
+    //     {/* 'far fa-times-circle' */}
+    //   </button>
+    // );
   };
 }
 
